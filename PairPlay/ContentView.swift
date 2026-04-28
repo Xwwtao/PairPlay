c59696d67bc8ee24d433aaacf83c3b7c7215fc60
Xwwtao <xwwtao@outlook.com>
2026-04-28 15:29:16 +0800

    Initial Commit

---
+++ PairPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PairPlay
+//
+//  Created by Wentao Xie on 2026/4/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PairPlay/PairPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairPlayApp.swift
+//  PairPlay
+//
+//  Created by Wentao Xie on 2026/4/28.
+//
+
+import SwiftUI
+
+@main
+struct PairPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
